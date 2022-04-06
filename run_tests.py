0414d025cc0186c572ecdb0980b14d4398e67f7c
Никита Рубинов <n.rubinov@corp.mail.ru>
2022-04-06 13:18:36 +0300

    Added negative test

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 
+from selenium import webdriver
 import unittest
 
+def expected_true():
+    return True
+
+class TestSimple(unittest.TestCase):
+
+    def test_expected_true(self):
+        self.assertEqual(expected_true(), True)
+    
+    def test_selenium_fake(self):
+        driver = webdriver.Chrome()
+        driver.get("http://www.python.org")
+        assert "Pythonnnnn" in driver.title
+
 if __name__ == '__main__':
-    pass
+    unittest.main()
