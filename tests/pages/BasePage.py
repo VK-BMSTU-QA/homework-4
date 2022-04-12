1f7eb73bc880ba4e60b479dcca1fa65556656c80
Александр Клонов <aleksandrklonov@MacBook-Air-Aleksandr.local>
2022-04-13 00:42:38 +0300

    Up waiting timings

--- tests/pages/BasePage.py
+++ tests/pages/BasePage.py
@@ -12,17 +12,18 @@ class BasePage(Urls):
     def __init__(self, driver) -> None:
         super().__init__()
         self.driver = driver
+        self.driver.implicitly_wait(30)
 
-    def wait_render(self, selector, timeout=10):
+    def wait_render(self, selector, timeout=60):
         return WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable((By.CSS_SELECTOR, selector)))
 
-    def wait_visible(self, selector, timeout=10):
+    def wait_visible(self, selector, timeout=60):
         return WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector)))
 
     def wait_redirect(self, url, timeout=60):
         return WebDriverWait(self.driver, timeout).until(EC.url_matches(url))
 
-    def wait_any_redirect(self, url='some', timeout=5):
+    def wait_any_redirect(self, url='some', timeout=20):
         return WebDriverWait(self.driver, timeout).until(EC.url_contains(url))
 
     def wait_until_innerhtml_changes_after_click(self, selector, timeout=60):
@@ -38,7 +39,7 @@ class BasePage(Urls):
 
     def is_exist(self, selector):
         try:
-            self.wait_visible(selector, 5)
+            self.wait_visible(selector, 20)
         except TimeoutException:
             return False
         return True
