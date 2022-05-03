d7ae5ac0ea0d7fe77f3affe15cd46f0f1eacb906
Кучерявенко Алексей Павлович <APKucheryavenko@sberbank.ru>
2022-05-03 15:01:57 +0300

    всё ещё MERJ: рефактор регистрации

--- run_tests.py
+++ run_tests.py
@@ -5,7 +5,7 @@ import unittest
 
 from tests.main_test import MainPageTest
 from tests.login_test import LoginTest
-from tests.register_test import NegativeRegisterTest, PositiveRegisterTest
+from tests.register_test import RegisterTest
 from tests.favorites_test import FavoritesTest
 from tests.profile_test import ProfilePageTest
 from tests.search_test import SearchPageTest
@@ -13,8 +13,7 @@ from tests.search_test import SearchPageTest
 if __name__ == '__main__':
     suite = unittest.TestSuite((
         unittest.makeSuite(LoginTest),
-        unittest.makeSuite(PositiveRegisterTest),
-        unittest.makeSuite(NegativeRegisterTest),
+        unittest.makeSuite(RegisterTest),
         unittest.makeSuite(MainPageTest),
         unittest.makeSuite(SearchPageTest),
         unittest.makeSuite(ProfilePageTest),
--- tests/register_test.py
+++ tests/register_test.py
@@ -25,33 +25,57 @@ class RegisterForm(Component):
     CONFIRM_PASSWORD = '//input[@name="confirm_password"]'
     REGISTER_BUTTON = '//input[@class="auth-form__submit"]'
     FRONTEND_WARNINGS = '//div[@class="auth-form__invalidities"]'
-    BACKEND_WARNINGS = 'auth-form__fail_msg'
+    BACKEND_WARNINGS_CLS = 'auth-form__fail_msg'
 
     def set_nickname(self, nickname):
-        self.driver.find_element_by_xpath(self.NICKNAME).send_keys(nickname)
+        input = WebDriverWait(self.driver, 10, 0.1).until(
+            lambda d: d.find_element_by_xpath(self.NICKNAME)
+        )
+        input.send_keys(nickname)
 
     def set_email(self, email):
-        self.driver.find_element_by_xpath(self.EMAIL).send_keys(email)
+        input = WebDriverWait(self.driver, 10, 0.1).until(
+            lambda d: d.find_element_by_xpath(self.EMAIL)
+        )
+        input.send_keys(email)
 
     def set_password(self, password):
-        self.driver.find_element_by_xpath(self.PASSWORD).send_keys(password)
+        input = WebDriverWait(self.driver, 10, 0.1).until(
+            lambda d: d.find_element_by_xpath(self.PASSWORD)
+        )
+        input.send_keys(password)
 
     def set_confirm_password(self, password):
-        self.driver.find_element_by_xpath(self.CONFIRM_PASSWORD).send_keys(password)
+        input = WebDriverWait(self.driver, 10, 0.1).until(
+            lambda d: d.find_element_by_xpath(self.CONFIRM_PASSWORD)
+        )
+        input.send_keys(password)
 
     def register(self):
-        self.driver.find_element_by_xpath(self.REGISTER_BUTTON).click()
+        button = WebDriverWait(self.driver, 10, 0.1).until(
+            lambda d: d.find_element_by_xpath(self.REGISTER_BUTTON)
+        )
+        button.click()
 
-    def frontend_warnings_text(self):
-        warnings = self.driver.find_element_by_xpath(self.FRONTEND_WARNINGS)
-        return warnings.text
+    def frontend_warnings(self):
+        warnings = WebDriverWait(self.driver, 10, 0.1).until(
+            lambda d: d.find_element_by_xpath(self.FRONTEND_WARNINGS)
+        )
+        return warnings
 
-    def backend_warnings_text(self):
-        warnings = self.driver.find_element_by_class_name(self.BACKEND_WARNINGS)
-        return warnings.text
+    def backend_warnings(self):
+        warnings = WebDriverWait(self.driver, 10, 0.1).until(
+            lambda d: d.find_element_by_class_name(self.BACKEND_WARNINGS_CLS)
+        )
+        return warnings
 
 
 class RegisterTest(unittest.TestCase):
+    NICKNAME = "test_{}".format(random.randint(0, 1000))
+    EMAIL = "test{}@test.com".format(random.randint(0, 1000))
+    SHORT_PASSWORD = "123"
+    PASSWORD = "djqowjdl12"
+
     def setUp(self):
         browser = os.environ.get('TESTBROWSER', 'CHROME')
 
@@ -59,69 +83,56 @@ class RegisterTest(unittest.TestCase):
             command_executor='http://127.0.0.1:4444/wd/hub',
             desired_capabilities=getattr(DesiredCapabilities, browser).copy()
         )
-
-    def tearDown(self):
-        self.driver.quit()
-
-    def test(self):
         self.register_page = RegisterPage(self.driver)
         self.register_page.open()
         self.register_form = self.register_page.form
 
+    def tearDown(self):
+        self.driver.quit()
 
-class PositiveRegisterTest(RegisterTest):
-    NICKNAME = "test_{}".format(random.randint(0, 1000))
-    EMAIL = "test{}@test.com".format(random.randint(0, 1000))
-    PASSWORD = "djqowjdl12"
-
-    def test(self):
-        super().test()
-
-        # Отсутствие ошибок при вводе верных реквизитов
+    def positive_test(self):
         self.register_form.set_nickname(self.NICKNAME)
         self.register_form.set_email(self.EMAIL)
         self.register_form.set_password(self.PASSWORD)
         self.register_form.set_confirm_password(self.PASSWORD)
 
-        assert not self.register_form.frontend_warnings_text()
-        assert not self.register_form.backend_warnings_text()
+        assert not self.register_form.frontend_warnings().text
+        assert not self.register_form.backend_warnings().text
 
         self.register_form.register()
 
-        # Успешная регистрация при вводе верных реквизитов
         WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, "avatar__img"))
         )
 
-
-class NegativeRegisterTest(RegisterTest):
-    NICKNAME = "test_nickname"
-    EMAIL = "test@test.com"
-    SHORT_PASSWORD = "123"
-    PASSWORD = "djqowjdl12"
-
-    def test(self):
-        super().test()
-
-        # Ошибка при submit'е пустой формы
+    def test_empty_form(self):
         self.register_form.register()
-        assert self.register_form.backend_warnings_text()
+        assert self.register_form.backend_warnings()
 
-        self.register_form.set_nickname(self.NICKNAME)
+    def test_empty_password(self):
+        self.register_form.set_email(self.EMAIL)
+        self.register_form.register()
+        assert self.register_form.backend_warnings()
 
-        # Ошибка при вводе некорректного email-адреса
+    def test_empty_email(self):
+        self.register_form.set_password(self.PASSWORD)
+        self.register_form.register()
+        assert self.register_form.backend_warnings()
+
+    def test_invalid_email(self):
         self.register_form.set_email(self.EMAIL.replace(".", ""))
+        self.register_form.set_password(self.PASSWORD)
         self.register_form.register()
-        assert self.register_form.frontend_warnings_text()
+        assert self.register_form.frontend_warnings()
 
-        # Ошибка при вводе пароля короче 8 символов
+    def test_invalid_password(self):
         self.register_form.set_email(self.EMAIL)
         self.register_form.set_password(self.SHORT_PASSWORD)
         self.register_form.set_confirm_password(self.SHORT_PASSWORD)
         self.register_form.register()
-        assert self.register_form.frontend_warnings_text()
+        assert self.register_form.frontend_warnings()
 
-        # Ошибка при вводе несовпадающих паролей
+    def test_non_similar_passwords(self):
         self.register_form.set_password(self.PASSWORD)
         self.register_form.register()
-        assert self.register_form.frontend_warnings_text()
+        assert self.register_form.frontend_warnings()
