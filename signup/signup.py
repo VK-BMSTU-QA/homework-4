5e344df43fcdafadd9dcaf758cc8d3892ae19a22
s.kust <s.kust@vk.team>
2022-05-04 19:37:00 +0300

    Refactor tests signup

--- signup/signup.py
+++ signup/signup.py
@@ -7,6 +7,7 @@ from signin.page import SignInPage
 from profile.page import ProfilePage
 from signup.page import SignUpPage
 from navbar.page import NavbarPage
+from signup.utils import TestUtils
 
 url = "https://goodvibesazot.tk/signup"
 
@@ -26,118 +27,78 @@ class Signup(unittest.TestCase):
         self.profilePage = ProfilePage(self.driver)
         self.signupPage = SignUpPage(self.driver)
         self.navbarPage = NavbarPage(self.driver)
+        self.testUtils = TestUtils(driver=self.driver)
+        self.driver.get(url=url)
 
     def test_signup_positive(self):
-        driver = self.driver
-        driver.get(url=url)
-        element_login = self.signupPage.get_login_element()
-        element_login.send_keys(LOGIN)
+        self.testUtils.fill_login(LOGIN)
 
-        element_email = self.signupPage.get_email_element()
-        element_email.send_keys(EMAIL)
+        self.testUtils.fill_email(EMAIL)
 
-        element_password = self.signupPage.get_password_element()
-        element_password.send_keys(PASSWORD)
+        self.testUtils.fill_password(PASSWORD)
 
-        element_password_repeat = self.signupPage.get_password_confirm_element()
-        element_password_repeat.send_keys(PASSWORD_CONFIRM)
+        self.testUtils.fill_password_confirm(PASSWORD_CONFIRM)
 
-        element_button = self.signupPage.get_button_element()
-        element_button.click()
+        self.testUtils.click_button()
 
-        profile_icon = self.navbarPage.get_profile_icon()
-        profile_icon.click()
-
-        profile_link = self.navbarPage.get_profile_link()
-        profile_link.click()
-
-        user_name = self.profilePage.get_username_element()
-        self.assertEqual(LOGIN, user_name.text)
+        user_name = self.testUtils.check_authorization()
+        self.assertEqual(LOGIN, user_name)
 
     def test_signup_empty_field(self):
-        driver = self.driver
-        driver.get(url=url)
+        self.testUtils.fill_login(LOGIN)
 
-        element_login = self.signupPage.get_login_element()
-        element_login.send_keys(LOGIN)
+        self.testUtils.click_button()
 
-        element_button = self.signupPage.get_button_element()
-        element_button.click()
-
-        error = self.signupPage.get_error_element()
-        self.assertEqual(error.text, 'Заполните все поля')
+        error = self.testUtils.wait_error()
+        self.assertEqual(error, 'Заполните все поля')
 
     def test_signup_user_exists(self):
-        driver = self.driver
-        driver.get(url=url)
-        element_login = self.signupPage.get_login_element()
-        element_login.send_keys(LOGIN_USER_EXISTS)
+        self.testUtils.fill_login(LOGIN)
 
-        element_email = self.signupPage.get_email_element()
-        element_email.send_keys(EMAIL)
+        self.testUtils.fill_email(EMAIL)
 
-        element_password = self.signupPage.get_password_element()
-        element_password.send_keys(PASSWORD)
+        self.testUtils.fill_password(PASSWORD)
 
-        element_password_repeat = self.signupPage.get_password_confirm_element()
-        element_password_repeat.send_keys(PASSWORD_CONFIRM)
+        self.testUtils.fill_password_confirm(PASSWORD_CONFIRM)
 
-        element_button = self.signupPage.get_button_element()
-        element_button.click()
+        self.testUtils.click_button()
 
-        error = self.signupPage.get_error_element()
-        self.assertEqual(error.text, 'Пользователь уже существует')
+        error = self.testUtils.wait_error()
+        self.assertEqual(error, 'Пользователь уже существует')
 
     def test_signup_passwords_not_match(self):
-        driver = self.driver
-        driver.get(url=url)
-        element_login = self.signupPage.get_login_element()
-        element_login.send_keys(LOGIN_USER_EXISTS)
+        self.testUtils.fill_login(LOGIN)
 
-        element_email = self.signupPage.get_email_element()
-        element_email.send_keys(EMAIL)
+        self.testUtils.fill_email(EMAIL)
 
-        element_password = self.signupPage.get_password_element()
-        element_password.send_keys(PASSWORD)
+        self.testUtils.fill_password(PASSWORD)
 
-        element_password_repeat = self.signupPage.get_password_confirm_element()
-        element_password_repeat.send_keys(PASSWORD_CONFIRM_NOT_MATCH)
+        self.testUtils.fill_password_confirm(PASSWORD_CONFIRM_NOT_MATCH)
 
-        element_button = self.signupPage.get_button_element()
-        element_button.click()
+        self.testUtils.click_button()
 
-        error = self.signupPage.get_error_element()
-        self.assertEqual(error.text, 'Пароли не одинаковые')
+        error = self.testUtils.wait_error()
+        self.assertEqual(error, 'Пароли не одинаковые')
 
     def test_signup_wrong_email_wrong(self):
-        driver = self.driver
-        driver.get(url=url)
-        element_login = self.signupPage.get_login_element()
-        element_login.send_keys(LOGIN_USER_EXISTS)
+        self.testUtils.fill_login(LOGIN)
 
-        element_email = self.signupPage.get_email_element()
-        element_email.send_keys(EMAIL_WRONG)
+        self.testUtils.fill_email(EMAIL_WRONG)
 
-        element_password = self.signupPage.get_password_element()
-        element_password.send_keys(PASSWORD)
+        self.testUtils.fill_password(PASSWORD)
 
-        element_password_repeat = self.signupPage.get_password_confirm_element()
-        element_password_repeat.send_keys(PASSWORD_CONFIRM)
+        self.testUtils.fill_password_confirm(PASSWORD)
 
-        element_button = self.signupPage.get_button_element()
-        element_button.click()
+        self.testUtils.click_button()
 
-        error = self.signupPage.get_error_element()
-        self.assertEqual(error.text, 'Неправильный формат данных')
+        error = self.testUtils.wait_error()
+        self.assertEqual(error, 'Неправильный формат данных')
 
     def test_signup_redirect_to_signin(self):
-        driver = self.driver
-        driver.get(url=url)
-        element_signup_link = self.signupPage.get_signin_link()
-        element_signup_link.click()
+        self.testUtils.click_on_signin_link()
 
-        signin_title = self.signinPage.get_signin_title()
-        self.assertEqual(signin_title.text, 'Вход')
+        signin_title = self.testUtils.check_signup_redirect()
+        self.assertEqual(signin_title, 'Вход')
 
     def tearDown(self):
         self.driver.close()
