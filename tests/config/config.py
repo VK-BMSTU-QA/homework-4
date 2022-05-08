1f94086b7f12a347eba744ba06002a0ee08ff147
Alexander Klonov <alexander.klonov@mail.ru>
2022-05-08 22:55:47 +0300

    Move config

---
+++ tests/config/config.py
@@ -0,0 +1,8 @@
+from attr import Factory
+
+
+ON_DRIVER = False
+DRIVER = './drivers/chromedriver_linux'
+BROWSER = 'chrome'
+NODE_URL = 'http://localhost:4444/wd/hub'
+MOZILA_DRIVER = './drivers/geckodriver'
